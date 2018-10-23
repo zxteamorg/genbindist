de02339080d32e68269470ba82801e40e6119474
Max Anurin <maxim.anurin@zxteam.org>
2018-10-23 23:35:25 +0300

    Add ability to use /etc/pkglist

--- pkg-update.sh
+++ pkg-update.sh
@@ -2,27 +2,35 @@
 #
 
 TS=$(date +'%Y%m%d%H%M%S')
+#WORKDIR=$(mktemp --directory --tmpdir update-world.$TS.XXXXXXXXXX)
 
-WORKDIR=$(mktemp --directory --tmpdir update-world.$TS.XXXXXXXXXX)
+echo "=== $TS ===" >> "/var/log/pkg-update.log"
+echo "=== $TS ===" >> "/var/log/pkg-update.err"
 
-
-emerge --sync > "$WORKDIR/phase1-sync-portage.log"
+emerge --sync >> "/var/log/pkg-update.log"
 if [ $? -ne 0 ]; then
- echo "Cannot sync portage tree" >&2
+ echo "Cannot sync portage tree" >> "/var/log/pkg-update.err"
  exit 1
 fi
 
-emerge --pretend --verbose --newuse --deep --update @world > "$WORKDIR/phase2-update-world.log" 
+emerge --verbose --newuse --deep --update @world >> "/var/log/pkg-update.log"
 if [ $? -ne 0 ]; then
- echo "Cannot update world" >&2
+ echo "Cannot update world" >> "/var/log/pkg-update.err"
  exit 2
 fi
 
-emerge @preserved-rebuild > "$WORKDIR/phase3-preserved-rebuild.log"
+if [ -f "/etc/pkglist" ]; then
+    cat /etc/pkglist | xargs emerge --verbose --newuse --deep --update --buildpkgonly >> "/var/log/pkg-update.log"
+    if [ $? -ne 0 ]; then
+    echo "Cannot update packages by /etc/pkglist" >> "/var/log/pkg-update.err"
+    exit 2
+  fi
+fi
+
+emerge @preserved-rebuild >> "/var/log/pkg-update.log"
 if [ $? -ne 0 ]; then
- echo "Cannot preserved-rebuild" >&2
+ echo "Cannot preserved-rebuild" >> "/var/log/pkg-update.err"
  exit 3
 fi
 
 echo -1 | etc-update
-
